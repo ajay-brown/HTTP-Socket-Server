3613c38d938e0aae9d6b84b8e9a2627d8839e29d
Audrey <audreybrown94@gmail.com>
2018-03-01 21:34:07 -1000

    inital page loading, need to handle favicon requests

--- client.js
+++ client.js
@@ -2,5 +2,11 @@ const net = require("net");
 
 const client = net.createConnection({ port: 8080 }, () => {
   console.log("You are connected");
-  client.write("Welcome");
+  client.on("data", data => {
+    process.stdin.pipe(client);
+    client.pipe(process.stdout);
+  });
+  client.on("end", () => {
+    console.log("Bye");
+  });
 });
--- server.js
+++ server.js
@@ -1,12 +1,59 @@
 const net = require("net");
+const fs = require("fs");
 
 const server = net.createServer(c => {
   console.log("client connected");
-  c.on("end", () => {
-    console.log("Client disconnected");
+  //   c.on("end", () => {
+  //     console.log("Client disconnected");
+  //   });
+  c.on("data", data => {
+    const dataString = data.toString();
+    console.log(dataString);
+
+    console.log(dataString.charAt(5));
+
+    switch (true) {
+      case dataString.charAt(5) === "i":
+        fs.readFile("./index.html", (err, fd) => {
+          if (err) throw err;
+          c.write("HTTP/1.1 200 OK\n\n");
+          console.log(fd.toString());
+          c.write(fd.toString());
+          c.end();
+        });
+        break;
+      case dataString.charAt(6) === "y":
+        fs.readFile("./hydrogen.html", (err, fd) => {
+          console.log(fd.toString());
+          c.write("HTTP/1.1 200 OK\n\n");
+          if (err) throw err;
+          c.end();
+        });
+        break;
+      case dataString.charAt(6) === "e":
+        fs.readFile("./helium.html", (err, fd) => {
+          console.log(fd.toString());
+          c.write("HTTP/1.1 200 OK\n\n");
+          if (err) throw err;
+          c.end();
+        });
+        break;
+      case dataString.charAt(5) === "s":
+        c.write("HTTP/1.1 200 OK\n\n Content-Type: text/css \n\n");
+        fs.readFile("./styles.css", (err, fd) => {
+          console.log(fd.toString());
+          c.write(fd.toString());
+          if (err) throw err;
+        });
+      default:
+        fs.readFile("./404.html", (err, fd) => {
+          console.log(fd.toString());
+          c.write("HTTP/1.1 200 OK\n\n");
+          if (err) throw err;
+          c.end();
+        });
+    }
   });
-  c.write("Welcome!");
-  c.pipe(c);
 });
 server.on("error", err => {
   throw err;
